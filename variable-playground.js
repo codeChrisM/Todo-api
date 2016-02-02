3f2a37e923450cb0f49ef758ce3f8a76c0874b0f
chris marlowe <chris@codechris.com>
2016-02-02 05:36:40 -0600

    add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+// var person= {
+// 	name: "Andrew",
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	// obj= {
+// 	// 	name: "Andrew",
+// 	// 	age: 24
+// 	// };
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+
+
+var array = [15,88]
+
+function updates (update){
+	update.push(13);
+
+	debugger;
+	update = [10,12,1];
+}
+
+updates(array);
+
+console.log(array);
